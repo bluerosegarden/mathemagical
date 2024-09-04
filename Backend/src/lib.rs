da3670b202f9b87eb9e60324c6cafdc74c7dd12d
Blue Rose <mail@bluerose.garden>
2024-09-04 16:16:56 -0500

    Experimenting before complete frontend rewrite

--- Backend/src/lib.rs
+++ Backend/src/lib.rs
@@ -12,8 +12,22 @@ pub fn greet(s: &str) {
     alert(&format!("{}", s));
 }
 
+#[wasm_bindgen]
+pub fn grab_string() -> String {
+    return "This Is A Strong String".to_string();
+}
 
 #[wasm_bindgen]
-pub fn grab_string() -> String{
-    return "This Is A Strong String".to_string();
+pub fn generate_math(
+    math_type: String,
+    problem_count: isize,
+    min_number: isize,
+    max_num: isize,
+    min_elements: isize,
+) -> String {
+    return format!(
+        "FROM RUST: {}\n\n{}\t{}\t{}\t{}",
+        math_type, problem_count, min_number, max_num, min_elements
+    )
+    .to_string();
 }
